6663f05c983284f33968ad1ef836e618f8f9160b
Sergey <rock-n-rolllll@bk.ru>
2021-04-05 12:42:58 +0300

    multiple configuration

--- demo-blog/demo-blog-web/src/main/java/ru/kpfu/itis/demo/blog/web/controller/ProfileController.java
+++ demo-blog/demo-blog-web/src/main/java/ru/kpfu/itis/demo/blog/web/controller/ProfileController.java
@@ -4,14 +4,21 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.kpfu.itis.demo.blog.web.security.details.UserDetailsImpl;
 
 @Controller
 public class ProfileController {
 
-    @GetMapping("/profile")
+    @GetMapping("/web/profile")
     public String getProfile(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
         model.addAttribute("user", userDetails);
         return "profile";
+    }
+
+    @GetMapping("/api/profile")
+    @ResponseBody
+    public String getProfile(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return userDetails.getUsername();
     }
 }
